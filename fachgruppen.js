1960f84f0f8d5b6233065abfe8e72c37b2314f9d
derjo <jonas@SH-Jonas-Mager.fritz.box>
2023-03-17 20:36:29 +0100

    Kinder und Jugenmedizin in den Fachgruppen ergänzt

--- fachgruppen.js
+++ fachgruppen.js
@@ -28,22 +28,22 @@ document.getElementById('Kinder').addEventListener("click", kinderClick, false);
 // Überschneidungen mit anderen callbackfunctions müssen verodert werden
 function gynaekologieClick() {
     document.getElementById('eHKS/FEK/oKFE').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked;
-    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
+    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked || document.getElementById('Kinder').checked;
     document.getElementById('DMP').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Psychiatrie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
 }
 function urologieClick() {
-    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
+    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked || document.getElementById('Kinder').checked;
 }
 function psychiatrieClick() {
     document.getElementById('DMP').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Psychiatrie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
 }
 function diabetologieClick() {
     document.getElementById('DMP').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Psychiatrie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
-    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
+    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked || document.getElementById('Kinder').checked;
 }
 function gastroenterologieClick() {
     document.getElementById('eHKS/FEK/oKFE').checked = document.getElementById('Gastroenterologie').checked;
-    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
+    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked || document.getElementById('Kinder').checked;
 }
 function orthopädieClick() {
     document.getElementById('BG').checked = document.getElementById('Orthopädie').checked || document.getElementById('Chirurgie').checked;
@@ -55,16 +55,16 @@ function chirurgieClick() {
 }
 function dermatologieClick() {
     document.getElementById('eHKS/FEK/oKFE').checked = document.getElementById('Dermatologie').checked;
-    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
+    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked || document.getElementById('Kinder').checked;
 }
 function innereClick() {
     document.getElementById('DMP').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Psychiatrie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
     document.getElementById('HZV').checked = document.getElementById('Innere').checked;
-    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
+    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked || document.getElementById('Kinder').checked;
 }
 function kardiologieClick() {
     document.getElementById('DMP').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Psychiatrie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
 }
 function kinderClick() {
-    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked;
+    document.getElementById('Früherkennung').checked = document.getElementById('Gynäkologie').checked || document.getElementById('Urologie').checked || document.getElementById('Diabetologie').checked || document.getElementById('Gastroenterologie').checked || document.getElementById('Dermatologie').checked || document.getElementById('Innere').checked || document.getElementById('Kardiologie').checked || document.getElementById('Kinder').checked;
 }
